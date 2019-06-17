98b9f237a6f36db95ea02eea24790a66c56a7519
xiaomubiao <viseek@gmail.com>
2019-06-17 11:25:37 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -30,7 +30,7 @@ jsproxy_config({
       lines: [
         // 实验中...
         // 参考 https://github.com/EtherDream/jsproxy/tree/master/cf-worker
-        'node-cfworker.etherdream.com:8443'
+        'proud-art-a072.ifree.workers.dev'
       ]
     }
   },
